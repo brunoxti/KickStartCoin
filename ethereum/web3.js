e346f5024557ee713d911d08261b50c07e48b7b8
Bruno Alves <bruno.xti@outlook.com>
2022-05-03 22:04:51 -0300

    add index and solution render inside server

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,7 +1,16 @@
 import Web3 from "web3";
- 
-window.ethereum.request({ method: "eth_requestAccounts" });
- 
-const web3 = new Web3(window.ethereum);
+
+let web3;
+
+if(typeof window !== "undefined" && typeof window.ethereum !== "undefined"){
+    window.ethereum.request({ method: "eth_requestAccounts" });
+    web3 = new Web3(window.web3.currentProvider);
+}else{
+    const provider = new Web3.providers.HttpProvider(
+        "https://rinkeby.infura.io/v3/e2614991c51942479ee0c8fafdff3f38"
+    );
+
+    web3 = new Web3(provider);
+}
  
 export default web3;
