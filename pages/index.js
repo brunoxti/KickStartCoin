9b65be72063c566f4411a71c49b6772ca6a221d3
Bruno Alves <bruno.xti@outlook.com>
2022-05-07 15:40:15 -0300

    add conpomponent and style css

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Button, Card } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
+import 'semantic-ui-css/semantic.min.css'
 
 class CampaignIndex extends Component{
 
@@ -9,8 +11,28 @@ class CampaignIndex extends Component{
         return {campaigns};
     }
 
+    renderCampaigns(){
+        const items = this.props.campaigns.map(address => {
+            return {
+                header: address,
+                description: <a>View Campain</a>,
+                fluid: true
+            };
+        });
+
+        return <Card.Group items={items}/>;
+    }
+
     render(){
-        return <div>{this.props.campaigns[0]}</div>
+        return <div>
+            {this.renderCampaigns()}
+            <Button
+            content="Create Campaign"
+            icon="add circle"
+            primary={true}
+            />
+            
+            </div>
     }
 }
 
