86b5b7a266ddd2abcb2420c3d3b4f39ac7d62838
Bruno <bruno.xti@outlook.com>
2022-08-12 19:13:40 -0300

    set address on .env

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0x1B92Ca143c5aA3231c96916fEB983d6E65A48cDD'
+    process.env.ADDRESS
 );
 
 export default instance;
